d65c35a99fa48cc950770042c4b10bd750a4cb69
Diego Ribeiro <dihrm2001@gmail.com>
2021-11-01 17:09:27 -0300

    fix: validaçao nos parametros falhava quando nao existia algum dos parametros

--- backend/src/validations/newTaskValidate.js
+++ backend/src/validations/newTaskValidate.js
@@ -1,6 +1,8 @@
 const errors = require('../errors/tasksErrors');
 
 module.exports = (info, status) => {
+  if (!status || !info) return { error: errors.invalidParams };
+
   if (
     status.trim() === ''
     || info.trim() === ''
