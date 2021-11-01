3dc7c9dee53bda70231e4450b7be91677527c638
Diego Ribeiro <dihrm2001@gmail.com>
2021-11-01 12:37:37 -0300

    create: conexao com Mongo configurada

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,1 +1,22 @@
 // Connection
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/Ollert';
+const DB_NAME = 'Ollert';
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = async () => {
+  if (db) Promise.resolve(db);
+
+  const connect = await MongoClient.connect(MONGO_DB_URL, OPTIONS);
+  db = await connect.db(DB_NAME);
+  return db;
+};
+
+module.exports = connection;
