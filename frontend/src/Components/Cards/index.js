52fa7ef8a4cc22f6bc45f3300e350c723b494703
Diego Ribeiro <dihrm2001@gmail.com>
2021-11-02 16:34:16 -0300

    create: componente Cards que realiza renderizaçao do card e no clique redireciona para pagina de ediçao da tarefa

---
+++ frontend/src/Components/Cards/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+export default function Cards({ tasks }) {
+  return (
+    tasks.map(({ _id: id, info }) => (
+      <Link to={`/${id}`} className="cardWrapper" key={id}>
+        <p className="cardInfo">{info}</p>
+      </Link>
+    ))
+  );
+}
