bd46dbbb1de97325fc4be099633ccde16a5872b6
Diego Ribeiro <dihrm2001@gmail.com>
2021-11-02 14:04:06 -0300

    create: header criado e estilizado

---
+++ frontend/src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="headerWrapper">
+      <h1 className="headerTitle">
+        Ollert - A sua lista de tarefas
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
